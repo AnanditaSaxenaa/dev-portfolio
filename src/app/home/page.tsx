538f083b65c9a180aca0df0b523469f6285d77e1
AnanditaSaxenaa <114992972+AnanditaSaxenaa@users.noreply.github.com>
2025-06-13 14:55:48 +0530

    Delete src/app/home directory

--- src/app/home/page.tsx
+++
@@ -1,9 +0,0 @@
-
-import { redirect } from 'next/navigation';
-
-export default function OldHomePage() {
-  redirect('/'); // Redirect to the root landing page
-  // Return null or an empty fragment as redirect is a server-side operation
-  // and this component won't render anything.
-  return null;
-}
